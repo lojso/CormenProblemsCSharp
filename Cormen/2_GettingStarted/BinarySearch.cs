945a5a1e3cbddfce7afb7f09310310e86b5c40f5
Nikita Grishaev <grishaev@rightway.games>
2021-03-06 01:33:51 +0300

    BinarySearch

---
+++ Cormen/2_GettingStarted/BinarySearch.cs
@@ -0,0 +1,54 @@
+﻿namespace Cormen._2_GettingStarted
+{
+    public static class BinarySearch
+    {
+        public static int Sort(int[] array, int value)
+        {
+            var low = 0;
+            var high = array.Length - 1;
+            while (low <= high)
+            {
+                var mid = (low + high) / 2;
+                
+                if(value == array[mid])
+                    return mid;
+
+                if (value < array[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int RSort(int[] array, int value)
+        {
+            return RSortImpl(array, value, 0, array.Length - 1);
+        }
+
+        private static int RSortImpl(int[] array, int value, int low, int high)
+        {
+            if (low >= high)
+                return -1;
+            
+            var mid = (low + high) / 2;
+            
+            if (value == array[mid])
+                return mid;
+
+            if (value < array[mid])
+            {
+                return RSortImpl(array, value, low, mid - 1);
+            }
+            else
+            {
+                return RSortImpl(array, value, mid + 1, high);
+            }
+        }
+    }
+}
